d912fb048fa09a4be0e0748b29fa0e9cc6d4abcf
имя фамилия < >
2026-09-13 17:31:22 +0300

    feat: редактирование файлов home и contacts в соответствии с шаблоном из задания, редактирование views

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 def home_view(request):
-    # Рендер шаблона главной страницы
     return render(request, 'catalog/home.html')
 
 def contacts_view(request):
     context = {}
     if request.method == 'POST':
         name = request.POST.get('name')
+        phone = request.POST.get('phone')  # Получение телефона
         message = request.POST.get('message')
-        # Вывод данных в консоль
-        print(f"Получено сообщение от {name}: {message}")
+
+        # Вывод в консоль
+        print(f"Новое обращение!\nИмя: {name}\nТелефон: {phone}\nСообщение: {message}\n---")
         context['message_sent'] = True
 
     return render(request, 'catalog/contacts.html', context)
